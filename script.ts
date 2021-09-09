427d5314e749ba0a1dda7df2294f705907cf80bf
Kyojo <sethfb@gmail.com>
2021-09-09 19:59:12 -0400

    actual work

--- script.ts
+++ script.ts
@@ -1,1 +1,14 @@
-const message="hi crinfarr"
+const message = "hi crinfarr";
+function fablesMenuShow(): void {
+  let bar = document.getElementsByClassName("fables");
+  for (let i = 0; i < bar.length; i++) {
+    let button = bar.item(i);
+    if (button != null) {
+      if (button.className == "fables") {
+        button.className += " hide";
+      } else {
+        button.className = "fables";
+      }
+    }
+  }
+}
