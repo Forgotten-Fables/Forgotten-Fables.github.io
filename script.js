e00449b34d9223255b0cb30a7c97aa61adefc40c
Kyojo <sethfb@gmail.com>
2021-09-09 20:03:31 -0400

    q

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
 var message = "hi crinfarr";
+function fablesMenuShow() {
+    var bar = document.getElementsByClassName("fables");
+    for (var i = 0; i < bar.length; i++) {
+        var button = bar.item(i);
+        if (button != null) {
+            if (button.className == "fables") {
+                button.className += " hide";
+            }
+            else {
+                button.className = "fables";
+            }
+        }
+    }
+}
